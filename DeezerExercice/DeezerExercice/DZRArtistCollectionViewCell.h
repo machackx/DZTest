4620db9a5a16a9dc176410fad82a35e50e0a2a31
Yang Ce <ce.yang@me.com>
2015-05-30 11:50:57 +0200

    Initial commit, untouched code

---
+++ DeezerExercice/DeezerExercice/DZRArtistCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  DZRArtistCollectionViewCell.h
+//  DeezerExercice
+//  Copyright (c) 2015 Deezer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZRArtistCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *artistImage;
+@property (weak, nonatomic) IBOutlet UILabel *artistName;
+
+@end
---
+++ DeezerExercice/DeezerExercice/DZRArtistCollectionViewCell.m
@@ -0,0 +1,11 @@
+//
+//  DZRArtistCollectionViewCell.m
+//  DeezerExercice
+//  Copyright (c) 2015 Deezer. All rights reserved.
+//
+
+#import "DZRArtistCollectionViewCell.h"
+
+@implementation DZRArtistCollectionViewCell
+
+@end
